e00d9f987356decd6f4c70120f63604cc39ad631
Yodaman07 <ayaan.irshad@gmail.com>
2025-03-12 11:25:54 -0700

    Updated design plan and can read both inputs, converting them to a 2d char array

--- P2_ayaan_irshad/src/Map.java
+++ P2_ayaan_irshad/src/Map.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 
 import DataStructures.Queue;
 
-public class Maze {
+public class Map {
 	
 	private String path;
 	private boolean coordBased;
@@ -16,13 +16,13 @@ public class Maze {
 	private int cols;
 	private int level;
 	
-	public Maze(String filePath, boolean coordBased) {
+	public Map(String filePath, boolean coordBased) {
 		this.path = filePath;
 		this.coordBased = coordBased;
 	}
 	
 	
-	public void loadMaze() {
+	public void load() {
 		try {
 			File f = new File(this.path);
 			Scanner s = new Scanner(f);
@@ -33,14 +33,34 @@ public class Maze {
 			this.cols = Integer.valueOf(data[1]);
 			this.level = Integer.valueOf(data[2]);
 			this.maze = new char[this.rows][this.cols];
-			System.out.println(rows + ":" + cols + ":" + level);
-			int r = 0;
-			while (s.hasNextLine() && r < this.rows) {
-				String line = s.nextLine(); 
-				for (int i = 0; i < this.cols; i++) {
-					this.maze[r][i] = line.charAt(i);
+//			System.out.println(rows + ":" + cols + ":" + level);
+			
+			if (!coordBased) { //loading regular map based mazes
+				int r = 0;
+				while (s.hasNextLine() && r < this.rows) {
+					String line = s.nextLine(); 
+					for (int i = 0; i < this.cols; i++) {
+						this.maze[r][i] = line.charAt(i);
+					}
+					r++;
 				}
-				r++;
+			}else { //loading coordinate based mazes
+				while (s.hasNextLine()) {
+					char elmn = s.next().toCharArray()[0]; //only 1 char
+					int row = s.nextInt();
+					int col = s.nextInt();
+					int lvl = s.nextInt();
+					System.out.println(elmn + " " + row + " " + col + " " + lvl);
+					this.maze[row][col] = elmn;
+				}
+				
+				for (int r = 0; r < this.rows; r++) {
+					for (int c = 0; c < this.cols; c++) {
+						if (this.maze[r][c] == '\u0000') {this.maze[r][c] = '.';}
+						
+					}
+				}
+				
 			}
 			
 			
@@ -50,7 +70,13 @@ public class Maze {
 		}
 	}
 	
+	
+	
+	
 	public void printMaze() {
+		if (this.maze == null) {System.out.println("You need to load a maze");}
+		
+		
 		for (int r = 0; r < this.rows; r++) {
 			for (int c = 0; c < this.cols; c++) {
 				System.out.print(this.maze[r][c]);
@@ -72,6 +98,8 @@ public class Maze {
 			}
 			System.out.println("");
 		}
+		
+		
 	}
 	
 	void solverHelper() {
--- P2_ayaan_irshad/src/p2.java
+++ P2_ayaan_irshad/src/p2.java
@@ -3,8 +3,8 @@ import java.util.Stack;
 public class p2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Maze m = new Maze("src/TEST01.txt", false);
-		m.loadMaze();
+		Map m = new Map("src/TEST10.txt", false);
+		m.load();
 		m.printMaze();
 //		m.solver();
 //		System.out.println(m.loadMaze());
